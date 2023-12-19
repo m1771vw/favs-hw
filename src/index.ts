8b3f93a6ad088d245a2166f78ff3a9072de2e536
William Yang <williamyang93@gmail.com>
2023-12-19 02:23:46 -0800

    Basic graphql implementation started

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,83 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
-}
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const message = greet('John');
-console.log(message);
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `
+  type User {
+    id: String!
+    firstName: String
+    lastName: String
+    phoneNumber: String
+    username: String!
+    created_at: DateTime!
+    updated_at: DateTime!
+    friendships: [Friendship!]!
+  }
+  
+  type Friendship {
+    id: Int!
+    user_id: String!
+    friend_user_id: String!
+    created_at: DateTime!
+    updated_at: DateTime!
+    requestor: User!
+    requested: User!
+  }
+  
+  type FriendRequest {
+    id: Int!
+    user_requestor_id: String!
+    user_requested_id: String!
+    status: String!
+    created_at: DateTime!
+    updated_at: DateTime!
+    requestor: User!
+    requested: User!
+  }
+  
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
